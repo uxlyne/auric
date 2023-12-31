d50bad8984ea9ace2d7570a12228f38ada2aea54
Amy Lyne <amy.lyne92@gmail.com>
2023-12-30 19:51:13 -0700

    Welcome Screen

--- src/pages/WelcomeScreen.js
+++ src/pages/WelcomeScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/WelcomeScreen.css';
+import '../styles/WelcomeScreen.css'; // Ensure this CSS file path is correct
 
 const WelcomePage = () => {
   return (
     <div className="welcome">
       <div className="particles-container">
+        {/* Generate 200 div elements for the particle effect */}
         {[...Array(200)].map((_, i) => (
           <div key={i} className="dust-particle"></div>
         ))}
@@ -18,7 +19,6 @@ const WelcomePage = () => {
         Translating emotions into colors<br />
         for clear communication.
       </p>
-
       <Link to="/">
         <button className="startButton">Continue</button>
       </Link>
@@ -27,6 +27,7 @@ const WelcomePage = () => {
 };
 
 export default WelcomePage;
+
 
 
 
