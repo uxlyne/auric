992df04c80ae2d4f390e05359f7d01bca092327e
Amy Lyne <amy.lyne92@gmail.com>
2023-12-30 18:03:50 -0700

    Welcome Screen Changes

--- src/components/Aura.js
+++ src/components/Aura.js
@@ -4,8 +4,9 @@ const Aura = () => {
   const style = {
     container: {
       display: 'flex',
-      justifyContent: 'center',
+      flexDirection: 'column',
       alignItems: 'center',
+      justifyContent: 'center',
       height: '100vh',
       position: 'relative',
     },
@@ -14,70 +15,79 @@ const Aura = () => {
       height: '500px',
       borderRadius: '50%',
       boxShadow: `
-        0 0 10px 5px rgba(255, 255, 255, 0.6), /* inner white glow */
-        0 0 15px 10px rgba(47, 79, 79, 0.5), /* middle blue-green glow */
-        0 0 20px 15px rgba(224, 255, 255, 0.3); /* outer light cyan glow */
+        0 0 10px 5px rgba(255, 255, 255, 0.6),
+        0 0 15px 10px rgba(47, 79, 79, 0.5),
+        0 0 20px 15px rgba(224, 255, 255, 0.3)
       `,
       zIndex: 1,
-      animation: 'pulse 2s infinite', // Name of the animation and duration
+      animation: 'pulse 2s infinite',
+    },
+    text: {
+      position: 'absolute',
+      zIndex: 2,
     },
     '@keyframes pulse': {
       '0%': {
         boxShadow: `
           0 0 10px 5px rgba(255, 255, 255, 0.6),
           0 0 15px 10px rgba(47, 79, 79, 0.5),
-          0 0 20px 15px rgba(224, 255, 255, 0.3);
+          0 0 20px 15px rgba(224, 255, 255, 0.3)
         `,
       },
       '50%': {
         boxShadow: `
           0 0 20px 10px rgba(255, 255, 255, 0.8),
           0 0 30px 15px rgba(47, 79, 79, 0.7),
-          0 0 40px 20px rgba(224, 255, 255, 0.5);
+          0 0 40px 20px rgba(224, 255, 255, 0.5)
         `,
       },
       '100%': {
         boxShadow: `
           0 0 10px 5px rgba(255, 255, 255, 0.6),
           0 0 15px 10px rgba(47, 79, 79, 0.5),
-          0 0 20px 15px rgba(224, 255, 255, 0.3);
+          0 0 20px 15px rgba(224, 255, 255, 0.3)
         `,
       },
     },
   };
 
-  // Styles need to be injected into the component since keyframes can't be inline
   return (
     <>
       <style>
         {`
           @keyframes pulse {
             0% {
-              box-shadow: 0 0 10px 5px rgba(255, 255, 255, 0.6),
-                          0 0 15px 10px rgba(47, 79, 79, 0.5),
-                          0 0 20px 15px rgba(224, 255, 255, 0.3);
+              box-shadow: 
+                0 0 10px 5px rgba(255, 255, 255, 0.6),
+                0 0 15px 10px rgba(47, 79, 79, 0.5),
+                0 0 20px 15px rgba(224, 255, 255, 0.3);
             }
             50% {
-              box-shadow: 0 0 20px 10px rgba(255, 255, 255, 0.8),
-                          0 0 30px 15px rgba(47, 79, 79, 0.7),
-                          0 0 40px 20px rgba(224, 255, 255, 0.5);
+              box-shadow: 
+                0 0 20px 10px rgba(255, 255, 255, 0.8),
+                0 0 30px 15px rgba(47, 79, 79, 0.7),
+                0 0 40px 20px rgba(224, 255, 255, 0.5);
             }
             100% {
-              box-shadow: 0 0 10px 5px rgba(255, 255, 255, 0.6),
-                          0 0 15px 10px rgba(47, 79, 79, 0.5),
-                          0 0 20px 15px rgba(224, 255, 255, 0.3);
+              box-shadow: 
+                0 0 10px 5px rgba(255, 255, 255, 0.6),
+                0 0 15px 10px rgba(47, 79, 79, 0.5),
+                0 0 20px 15px rgba(224, 255, 255, 0.3);
             }
           }
         `}
       </style>
       <div style={style.container}>
         <div style={style.eclipse}></div>
+        <h1 style={style.text} className="glowing-text">Aura</h1>
       </div>
     </>
   );
 };
 
 export default Aura;
+
+
 
 
 
--- src/pages/WelcomeScreen.js
+++ src/pages/WelcomeScreen.js
@@ -1,12 +1,64 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/WelcomeScreen.css';
 
-const WelcomeScreen = () => {
+const WelcomePage = () => {
   return (
-    <div>
-      <h1>Welcome to Auric</h1>
-      {/* Other home page content */}
+    <div className="welcome">
+      <div className="particles-container">
+        {[...Array(100)].map((_, i) => (
+          <div key={i} className="dust-particle"></div>
+        ))}
+      </div>
+      <div className="eclipse"></div>
+      <div className="auraTitleContainer">
+        <h1 className="auraTitle">AURA</h1>
+      </div>
+      <p className="tagline">
+        Translating emotions into colors<br />
+        for clear communication.
+      </p>
+
+      <Link to="/">
+        <button className="startButton">Continue</button>
+      </Link>
     </div>
   );
 };
 
-export default WelcomeScreen;
+export default WelcomePage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
