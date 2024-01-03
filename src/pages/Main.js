af391d51b86e9660d0bb3acc74350e70433071b0
Amy Lyne <amy.lyne92@gmail.com>
2024-01-02 18:19:22 -0700

    Main Dark Background

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,29 +4,43 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import TextInput from '../components/TextInput';
 import TextAnalysis from '../components/TextAnalysis';
-import '../styles/Main.css';
+import '../styles/Main.css'; // Your existing Main.css
 
 const Main = () => {
   return (
     <div>
-    <Header />
-    <div className="main-container">
-      <div className="content">
-        <div className="left-panel">
-          <Aura />
+      <Header />
+      <div className="main-container">
+        {/* Add the particle container for the background effect */}
+        <div className="particle-container">
+          {[...Array(200)].map((_, i) => (
+            <div
+              key={i}
+              className="particle"
+              style={{
+                '--random-delay': Math.random(), // Assign a random delay
+              }}
+            ></div>
+          ))}
         </div>
-        <div className="right-panel">
-          <TextInput />
-          <TextAnalysis />
+
+        <div className="content">
+          <div className="left-panel">
+            <Aura />
+          </div>
+          <div className="right-panel">
+            <TextInput />
+            <TextAnalysis />
+          </div>
         </div>
       </div>
-    </div>
-    <Footer />
+      <Footer />
     </div>
   );
 }
 
 export default Main;
+
 
 
 
